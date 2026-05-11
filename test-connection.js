ae970e40c6e8ffc5686daff4eabff5eb68421a81
Hwnger <duyhungls2005@gmail.com>
2026-05-11 13:37:59 +0700

    first commit

--- test-connection.js
+++ test-connection.js
@@ -1,8 +1,6 @@
 // test-connection.js
 const mongoose = require('mongoose');
 
-// Thay thế bằng connection string của bạn
-const MONGODB_URI = 'mongodb+srv://duyhungls2005:08012005@cluster0.4riupee.mongodb.net/?appName=Cluster0';
 
 mongoose.connect(MONGODB_URI)
   .then(() => {
